52f9cdd567337fe59c8afa913d32c60d49f53aa5
aoba <ipbdipbd00@gmail.com>
2020-03-03 23:45:23 +0900

    Add fetch posts method

--- api/models/post.go
+++ api/models/post.go
@@ -1,10 +1,20 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"api/db"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Post struct {
 	gorm.Model
 	Title    string
 	ImageURL string
 	UserID   uint
+}
+
+func (p *Post) FetchPosts(limit int) []Post {
+	var posts []Post
+	db.Db.Order("created_at asc").Limit(limit).Find(&posts)
+	return posts
 }
--- api/routers/v1/post.go
+++ api/routers/v1/post.go
@@ -1,12 +1,19 @@
 package v1
 
 import (
+	"api/models"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetPosts(c *gin.Context) {
+	var post models.Post
+
+	posts := post.FetchPosts(10)
+	fmt.Println("posts: ", posts)
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "ok",
 	})
