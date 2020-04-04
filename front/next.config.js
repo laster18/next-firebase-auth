f155a9e3e401959ccb44f0a0c7fe6101458b66f9
aoba <ipbdipbd00@gmail.com>
2020-04-05 02:17:37 +0900

    [Front] Setting ant design

--- front/next.config.js
+++ front/next.config.js
@@ -1,10 +1,45 @@
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require('dotenv-webpack')
 const path = require("path")
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
 const withCSS = require('@zeit/next-css')
+const withLess = require('@zeit/next-less')
 
-module.exports = withCSS({
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+)
+
+module.exports = withLess(withCSS({
+  // for less settings
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables, // make your antd custom effective
+  },
   target: 'serverless',
-  webpack(config) {
+  webpack: (config, { isServer }) => {
+    // for less settings
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      })
+    }
+    // Next.js 9.1からpublicディレクトリがサポートされたので変更必要？
     config.module.rules.push({
       test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
       use: {
@@ -17,10 +52,12 @@ module.exports = withCSS({
         }
       }
     })
+    // directory root alias
     config.resolve.alias = {
       ...config.resolve.alias,
       "~": path.resolve(__dirname, "./src")
     };
+    // dotEnv settings
     config.plugins = [
       ...config.plugins,
       new Dotenv({
@@ -29,4 +66,4 @@ module.exports = withCSS({
     ];
     return config
   }
-})
+}))
